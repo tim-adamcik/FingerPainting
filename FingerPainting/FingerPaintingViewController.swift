5d5c082b58e5f83a4cda00d6c70dc7a799443ab8
Timothy Adamcik <tadamcik0425@gmail.com>
2020-05-18 14:19:35 -0400

    added helper functions to draw lines

--- FingerPainting/FingerPaintingViewController.swift
+++ FingerPainting/FingerPaintingViewController.swift
@@ -13,12 +13,75 @@ class FingerPaintingViewController: UIViewController {
     @IBOutlet weak var redoBtn: UIButton!
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var settingsBtn: UIButton!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    
+    var lastPoint = CGPoint.zero
+    var color = UIColor.black
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint ) {
+        UIGraphicsBeginImageContext(view.frame.size)
 
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        tempImageView.image?.draw(in: view.bounds)
+        
+        context.move(to: fromPoint)
+        context.move(to: toPoint)
+        
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setLineWidth(brushWidth)
+        context.setStrokeColor(color.cgColor)
+        
+        context.strokePath()
+        
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        swiped = true
+        let currentPoint = touch.location(in: view)
+        drawLine(from: lastPoint, to: currentPoint)
+        
+        lastPoint = currentPoint
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+        
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: 1.0)
+        tempImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: opacity)
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        tempImageView.image = nil
+
+    }
     @IBAction func undoBtnPressed(_ sender: Any) {
     }
     
