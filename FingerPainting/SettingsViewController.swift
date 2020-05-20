9b7c855a2de9241c673550c289c3beb09cbe2b32
Timothy Adamcik <tadamcik0425@gmail.com>
2020-05-20 12:46:25 -0400

    added brush width and opacity to preview image

--- FingerPainting/SettingsViewController.swift
+++ FingerPainting/SettingsViewController.swift
@@ -25,15 +25,47 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var customColorLabel: UIImageView!
     
+    var brush: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        brushWidthLabel.text = "\(brushSlider.value)"
+        opacityLevelLabel.text = "\(opacitySlider.value)"
     }
     
+    func drawPreview() {
+        UIGraphicsBeginImageContext(samplePenImageView.frame.size)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        context.setLineCap(.round)
+        context.setLineWidth(brush)
+        context.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: opacity).cgColor)
+        
+        context.move(to: CGPoint(x: samplePenImageView.bounds.height/2, y: samplePenImageView.bounds.width/2))
+        context.addLine(to: CGPoint(x: samplePenImageView.bounds.height/2, y: samplePenImageView.bounds.width/2))
+        context.strokePath()
+        samplePenImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    
     @IBAction func brushSliderMoved(_ sender: Any) {
+        brush = CGFloat(brushSlider.value)
+        brushWidthLabel.text = String(format: "%.1f", brush)
+        drawPreview()
+
     }
     
     @IBAction func opacitySliderMoved(_ sender: Any) {
+        opacity = CGFloat(opacitySlider.value)
+        opacityLevelLabel.text = String(format: "%.1f", opacity)
+        drawPreview()
     }
     @IBAction func redSliderMoved(_ sender: Any) {
     }
